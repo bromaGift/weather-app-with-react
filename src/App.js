865ce127b692333be8bf106fb26d5cdd41c5f16f
Broma Gift <elenwogift2@gmail.com>
2023-07-24 20:56:20 +0100

    style fixes

--- src/App.js
+++ src/App.js
@@ -1,41 +1,44 @@
-import React from 'react';
-import Title from "./components/Title"
-import Form from "./components/Form"
-import Weather from "./components/Weather"
-
+import React from "react";
+import Title from "./components/Title";
+import Form from "./components/Form";
+import Weather from "./components/Weather";
 
 class App extends React.Component {
-  
-    state= {
-      temperature: undefined,
-      city: undefined,
-      country: undefined,
-      longitude: undefined,
-      latitude: undefined,
-      description: undefined,
-      error: undefined
-    }
+  state = {
+    temperature: undefined,
+    city: undefined,
+    country: undefined,
+    longitude: undefined,
+    latitude: undefined,
+    description: undefined,
+    weather: undefined,
+    error: undefined,
+  };
 
-    
-    getWeather = async (e) => {
+  getWeather = async (e) => {
     e.preventDefault();
     const city = e.target.elements.city.value;
     const country = e.target.elements.country.value;
-    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}
-        &APPID=48ea13dce4ea18eb8644a93b6600cc4c&units=metric`);
+    const api_call =
+      await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}
+        &APPID=b9be2ac3359d379601590bb23a6e2952`);
+
     const data = await api_call.json();
 
+    console.log({ data });
+
     if (city && country) {
-      if (data.cod == 404) {
+      if (data.cod === 404) {
         this.setState({
-            temperature: undefined,
-            city: undefined,
-            country: undefined,
-            humidity: undefined,
-            description: undefined,
-            error: "Input doesn't match any known location!"
-        }); 
-    }
+          temperature: undefined,
+          city: undefined,
+          country: undefined,
+          humidity: undefined,
+          description: undefined,
+          weather: undefined,
+          error: "Input doesn't match any known location!",
+        });
+      }
       this.setState({
         temperature: data.main.temp,
         city: data.name,
@@ -43,8 +46,9 @@ class App extends React.Component {
         longitude: data.coord.lon,
         latitude: data.coord.lat,
         description: data.weather[0].description,
-        error: ""
-    });
+        weather: data.weather[0].main,
+        error: "",
+      });
     } else {
       this.setState({
         temperature: undefined,
@@ -53,44 +57,42 @@ class App extends React.Component {
         longitude: undefined,
         latitude: undefined,
         description: undefined,
-        error: "please enter your city and country"
+        weather: undefined,
+        error: "please enter your city and country",
       });
     }
-}
+  };
 
   render() {
     return (
       <div>
-
-       <div className="wrapper">
-         <div className="main">
-           <div className="container">
-             <div className="row">
-               <div className="col-5 title-container">
-                < Title />
-               </div>
-               <div className="col-7 form-container">
-                  <Form getWeather={this.getWeather}/>
-                    <Weather 
-                      temperature={this.state.temperature}
-                      longitude ={this.state.longitude}
-                      latitude={this.state.latitude}
-                      city={this.state.city}
-                      country={this.state.country}
-                      description={this.state.description}
-                      error={this.state.error}
-                    />
-               </div>
-             </div>
-           </div>
-         </div>
-       </div>
+        <div className="wrapper">
+          <div className="main">
+            <div className="container">
+              <div className="row">
+                <div className="col-5 title-container">
+                  <Title />
+                </div>
+                <div className="col-7 form-container">
+                  <Form getWeather={this.getWeather} />
+                  <Weather
+                    temperature={this.state.temperature}
+                    longitude={this.state.longitude}
+                    latitude={this.state.latitude}
+                    city={this.state.city}
+                    country={this.state.country}
+                    description={this.state.description}
+                    weather={this.state.weather}
+                    error={this.state.error}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
 }
-
-
-
 
 export default App;
--- src/components/Weather.js
+++ src/components/Weather.js
@@ -1,40 +1,60 @@
-import React from "react"
+import React from "react";
 
 function Weather(props) {
-        const {city, country, temperature, latitude, longitude, description, error} = props
-        return (
-            <div className="weather__info">
-                {
-                    props.city && props.country && <p className="weather__key">Location: 
-                   <span className="weather__value"> {city}, {country}</span>
-                   </p>
-                }
-                {
-                    props.longitude && <p className="weather__key">longitude: 
-                    <span className="weather__value"> {longitude}</span>
-                    </p>
-                }
-                
-                {
-                    props.latitude && <p className="weather__key">latitude:
-                   <span className="weather__value"> {latitude}</span>
-                   </p>
-                }
-                {
-                    props.temperature && <p className="weather__key">Temperature:
-                    <span className="weather__value"> {temperature} c</span>
-                     </p>
-                }
-                {
-                    props.description && <p className="weather__key">Description:
-                     <span className="weather__value"> {description}</span>
-                     </p>
-                }
-                {
-                    props.error && <p className="weather__error">{error}</p>
-                }
-            </div>
-        )
+  const {
+    city,
+    country,
+    temperature,
+    latitude,
+    longitude,
+    description,
+    weather,
+    error,
+  } = props;
+  return (
+    <div className="weather__info">
+      {props.city && props.country && (
+        <p className="weather__key">
+          Location:{" "}
+          <span className="weather__value">
+            {city}, {country}
+          </span>
+        </p>
+      )}
+      {props.longitude && (
+        <p className="weather__key">
+          longitude:
+          <span className="weather__value"> {longitude}</span>
+        </p>
+      )}
+
+      {props.latitude && (
+        <p className="weather__key">
+          latitude:
+          <span className="weather__value"> {latitude}</span>
+        </p>
+      )}
+      {props.temperature && (
+        <p className="weather__key">
+          Temperature:
+          <span className="weather__value"> {temperature} c</span>
+        </p>
+      )}
+      {props.weather && (
+        <p className="weather__key">
+          weather:
+          <span className="weather__value"> {weather}</span>
+        </p>
+      )}
+      {props.description && (
+        <p className="weather__key">
+          Description:
+          <span className="weather__value"> {description}</span>
+        </p>
+      )}
+      {props.error && <p className="weather__error">{error}</p>}
+    </div>
+  );
 }
 
-export default Weather
+export default Weather;
